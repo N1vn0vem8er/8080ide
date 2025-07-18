ca34985b7feedf65f882c9173b1c51ea336d8159
N1vn0vem8er <fq0xveupd5d@outlook.com>
2025-07-18 15:12:36 +0200

    Fixed globals.h

--- src/globals.h
+++ src/globals.h
@@ -3,7 +3,6 @@
 
 #include <QList>
 #include <QString>
-#endif // GLOBALS_H
 
 namespace Globals
 {
@@ -16,7 +15,28 @@ struct FileMemoryStartEnd
     FileMemoryStartEnd(QString fileName, unsigned short start, unsigned short end) : fileName(fileName), start(start), end(end){}
 };
 
+struct SimStatus{
+    SimStatus(){};
+    SimStatus(const QString& areg, const QString& breg, const QString& creg, const QString& dreg, const QString& ereg, const QString& hreg, const QString& lreg,
+              const QString& pc, const QString& sp, const QString& flags, const QString& mreg) :
+        areg(areg), breg(breg), creg(creg), dreg(dreg), ereg(ereg), hreg(hreg), lreg(lreg), pc(pc), sp(sp), flags(flags), mreg(mreg){};
+    QString areg;
+    QString breg;
+    QString creg;
+    QString dreg;
+    QString ereg;
+    QString hreg;
+    QString lreg;
+    QString pc;
+    QString sp;
+    QString flags;
+    QString mreg;
+    auto operator<=>(const SimStatus&) const = default;
+};
+
 const QString SETTINGSFILENAME = "settings.ini";
 const QString SETTINGSDIRNAME = "8080ide";
 
 }
+
+#endif // GLOBALS_H
