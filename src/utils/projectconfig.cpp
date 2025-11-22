4bf8d5d07b574af262c4e669c32bee5141a03955
N1vn0vem8er <fq0xveupd5d@outlook.com>
2025-11-22 16:25:08 +0100

    Implemented ProjectConfig::fromQString

--- src/utils/projectconfig.cpp
+++ src/utils/projectconfig.cpp
@@ -45,7 +45,33 @@ void ProjectConfig::fromFile(const QString &path)
 
 void ProjectConfig::fromQString(const QString &text)
 {
-
+    if(text.isEmpty()) return;
+    QJsonParseError parseError;
+    const QJsonDocument document = QJsonDocument::fromJson(text.toUtf8(), &parseError);
+    if(parseError.error == QJsonParseError::NoError)
+    {
+        const QJsonObject object = document.object();
+        if(object.contains("version"))
+            version = object.value("version").toString();
+        if(object.contains("name"))
+            name = object.value("name").toString();
+        if(object.contains("memorySize"))
+            memorySize = object.value("memorySize").toInt();
+        if(object.contains("startAt"))
+            startAt = object.value("startAt").toInt();
+        if(object.contains("stackPointer"))
+            stackPointer = object.value("stackPointer").toInt();
+        if(object.contains("files"))
+        {
+            const QJsonArray files = object.value("files").toArray();
+            for(const auto& file : files)
+            {
+                const QJsonObject obj = file.toObject();
+                if(obj.contains("filePath") && obj.contains("fileLocation"))
+                    filesInMemory.append({obj.value("filePath").toString(), obj.value("fileLocation").toInt()});
+            }
+        }
+    }
 }
 
 void ProjectConfig::toFile(const QString &path)
