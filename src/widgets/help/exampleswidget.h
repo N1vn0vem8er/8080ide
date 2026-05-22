2bbea5b26f9a14ab15caeb57343ef59835868a81
N1vn0vem8er <fq0xveupd5d@outlook.com>
2026-05-22 18:56:27 +0200

    Added drawing line example

--- src/widgets/help/exampleswidget.h
+++ src/widgets/help/exampleswidget.h
@@ -19,8 +19,8 @@ public:
 private:
     Ui::ExamplesWidget *ui;
     QString language;
-    QStringList helpPagesList = {"Hello World", "Hello World 2"};
-    QStringList translations = {tr("Hello World"), tr("Hello World 2")};
+    QStringList helpPagesList = {"Hello World", "Hello World 2", "Draw line"};
+    QStringList translations = {tr("Hello World"), tr("Hello World 2"), tr("Narysuj linie")};
 };
 
 #endif // EXAMPLESWIDGET_H
