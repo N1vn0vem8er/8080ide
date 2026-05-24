d48c84103477ad446484601a6898f1b9c457a6dd
N1vn0vem8er <fq0xveupd5d@outlook.com>
2026-05-24 17:45:00 +0200

    Added random number generator example

--- src/widgets/help/exampleswidget.h
+++ src/widgets/help/exampleswidget.h
@@ -19,8 +19,8 @@ public:
 private:
     Ui::ExamplesWidget *ui;
     QString language;
-    QStringList helpPagesList = {"helloworld", "helloworld2", "drawline", "drawcircle", "drawfigure", "fillscreen", "fillscreenwithcolors"};
-    QStringList translations = {tr("Hello World"), tr("Hello World 2"), tr("Draw line"), tr("Draw circle"), tr("Draw figure"), tr("Fill screen"), tr("Fill screen with colors")};
+    QStringList helpPagesList = {"helloworld", "helloworld2", "drawline", "drawcircle", "drawfigure", "fillscreen", "fillscreenwithcolors", "generaterandomnumber"};
+    QStringList translations = {tr("Hello World"), tr("Hello World 2"), tr("Draw line"), tr("Draw circle"), tr("Draw figure"), tr("Fill screen"), tr("Fill screen with colors"), tr("Generate random number")};
 };
 
 #endif // EXAMPLESWIDGET_H
