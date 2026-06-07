28cee7907da6872ccdb1f56fa788c1e37ad5dff4
N1vn0vem8er <fq0xveupd5d@outlook.com>
2026-06-07 18:59:52 +0200

    Changed helpidewidget

--- src/widgets/help/helpidewidget.cpp
+++ src/widgets/help/helpidewidget.cpp
@@ -1,39 +1,53 @@
 #include "helpidewidget.h"
-#include "qdiriterator.h"
-#include "qstringlistmodel.h"
 #include "ui_helpidewidget.h"
+
+#include <QFile>
 
 HelpIDEWidget::HelpIDEWidget(QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::HelpIDEWidget)
+    , model(new QStringListModel)
 {
     ui->setupUi(this);
-    QStringListModel* model = new QStringListModel(ui->listView);
-    model->setStringList(translations);
+    languageSuffix = (QLocale::system().name().startsWith("pl")) ? "_pl.html" : "_en.html";
+    initTranslations();
     ui->listView->setModel(model);
-    connect(ui->listView, &QListView::clicked, this, [&](const QModelIndex& index){openInstructionHelp(helpPagesList[index.row()].toLower());});
-    QLocale l;
-    language = l.name() == "pl_PL" ? "_pl.html" : "_en.html";
+
+    connect(ui->listView, &QListView::clicked, this, &HelpIDEWidget::onInstructionClicked);
 }
 
 HelpIDEWidget::~HelpIDEWidget()
 {
     delete ui;
 }
-void HelpIDEWidget::openInstructionHelp(QString instruction) const
+void HelpIDEWidget::openInstructionHelp(const QString &instruction)
 {
-    QDirIterator iterator(":/ide/helpPages/ide", QDirIterator::Subdirectories);
-    while(iterator.hasNext())
-    {
-        QString fileName = iterator.next();
-        if(fileName == ":/ide/helpPages/ide/" + instruction + language)
-        {
-            QFile file(fileName);
-            if(file.open(QFile::ReadOnly))
-            {
-                ui->textBrowser->setText(file.readAll());
-                file.close();
-            }
-        }
-    }
+    QString filePath = QString(":/ide/helpPages/ide/%1%2").arg(instruction.toLower(), languageSuffix);
+
+    QFile file(filePath);
+    if(file.open(QFile::ReadOnly | QFile::Text))
+        ui->textBrowser->setHtml(file.readAll());
+    else
+        ui->textBrowser->setText(tr("Help file not found."));
+}
+
+void HelpIDEWidget::onInstructionClicked(const QModelIndex &index)
+{
+    if(!index.isValid() || index.row() >= helpPagesList.size())
+        return;
+
+    openInstructionHelp(helpPagesList[index.row()]);
+}
+
+void HelpIDEWidget::initTranslations()
+{
+    QStringList translations{
+        tr("Welcome"),
+        tr("Projects"),
+        tr("Assembler"),
+        tr("Labels"),
+        tr("Graphics Screen"),
+        tr("Random Number Generator")
+    };
+    model->setStringList(translations);
 }
--- src/widgets/help/helpidewidget.h
+++ src/widgets/help/helpidewidget.h
@@ -1,6 +1,7 @@
 #ifndef HELPIDEWIDGET_H
 #define HELPIDEWIDGET_H
 
+#include <QStringListModel>
 #include <QWidget>
 
 namespace Ui {
@@ -14,13 +15,19 @@ class HelpIDEWidget : public QWidget
 public:
     explicit HelpIDEWidget(QWidget *parent = nullptr);
     ~HelpIDEWidget();
-    void openInstructionHelp(QString instruction) const;
+    void openInstructionHelp(const QString& instruction);
+
+private slots:
+    void onInstructionClicked(const QModelIndex &index);
+
+private:
+    void initTranslations();
 
 private:
     Ui::HelpIDEWidget *ui;
-    QString language;
-    QStringList helpPagesList = {"Welcome", "Projects", "Assembler", "Labels", "Graphics_Screen", "random_number_generator"};
-    QStringList translations = {tr("Welcome"), tr("Projects"), tr("Assembler"), tr("Labels"), tr("Graphics Screen"), tr("Random Number Generator")};
+    QStringListModel* model;
+    QString languageSuffix;
+    QStringList helpPagesList{"Welcome", "Projects", "Assembler", "Labels", "Graphics_Screen", "random_number_generator"};
 };
 
 #endif // HELPIDEWIDGET_H
